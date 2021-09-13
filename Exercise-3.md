30a7b483ec3d53f89efc8dc357b9061c8168a890
Ankur Gupta <ankur.gupta@logmein.com>
2021-09-13 07:35:03 +0200

    Added exercise 3

---
+++ Exercise-3.md
@@ -0,0 +1,7 @@
+# Account manager
+
+> two types of accounts: user and admin
+
+- Design an interface for each, given that users have email, password and isActive properties, and admins additionally have an adminSince property, which is of type Date.
+- export these interfaces from account-manager.ts moudle as IUser and IAdmin
+- Update the Account manager class you have been given, such that any mismatching is caught by the Typescript compiler at build time.
--- src/examples.ts
+++ src/examples.ts
@@ -11,3 +11,24 @@ class Bar {
 }
 
 let foo: Foo = new Bar();
+
+let teacherAge: number = 34;
+let teacherName: string = "mme";
+
+let input = document.querySelector("input#name_field") as HTMLInputElement;
+
+interface ClickListener {
+  (this: Window, e: MouseEvent): void;
+}
+
+const myListener: ClickListener = (e) => {
+  console.log("mouse clicked", e);
+};
+
+addEventListener("click", myListener);
+
+myListener.bind(window, new MouseEvent("click"));
+
+// Error the 'this' context of type
+//'void' is not asignable to
+//method's 'this' of type window
